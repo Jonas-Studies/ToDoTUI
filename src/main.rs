7854b650a5d328b2c88fa9217befdb39b503a10f
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-17 19:30:07 +0100

    Made use of the edit one task app

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod task;
 mod storage;
+mod tui;
 
 use task::Task;
 
 fn main() {
-    let tasks = if let Some(stored_tasks) = storage::get_tasks() {
+    let mut tasks = if let Some(stored_tasks) = storage::get_tasks() {
         stored_tasks
     }
     else {
         vec![ Task::new(String::from("Programm schreiben")) ]
     };
+
+    tui::edit_one_task(& mut tasks[0]);
 
     storage::set_tasks(& tasks);
 }
