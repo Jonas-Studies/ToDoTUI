84e7425e585166e189c6cd1469c4aa825c96366d
Jonas-Desktop <programming@jonas.zengerle.email>
2024-11-18 22:47:02 +0100

    Added the CanBeFocused trait as requirement to the IsContent trait

--- src/tui/content.rs
+++ src/tui/content.rs
@@ -1,11 +1,17 @@
-use super::traits::IsContent;
+use super::traits::{IsContent, CanBeFocused};
 
 use ratatui::{
     prelude::{Buffer, Rect},
-    widgets::{WidgetRef, Block, Paragraph}
+    widgets::{WidgetRef, Block, Paragraph},
+    style::{Style, Color}
 };
 
 impl IsContent for Paragraph <'_> {}
+
+impl CanBeFocused for Paragraph <'_> {
+    fn focus(& mut self) {}
+    fn unfocus(& mut self) {}
+}
 
 pub struct TextField <'textfields_lifetime> {
     text: String,
@@ -25,5 +31,14 @@ impl IsContent for TextField <'_> {}
 impl WidgetRef for TextField <'_> {
     fn render_ref(& self, area: Rect, buffer: & mut Buffer) {
         Paragraph::new(self.text.clone()).block(self.borders.clone()).render_ref(area, buffer);
+    }
+}
+
+impl CanBeFocused for TextField <'_> {
+    fn focus(& mut self) {
+        self.borders = self.borders.clone().border_style(Style::new().fg(Color::Yellow));
+    }
+    fn unfocus(& mut self) {
+        self.borders = self.borders.clone().style(Style::default());
     }
 }
--- src/tui/traits.rs
+++ src/tui/traits.rs
@@ -1,3 +1,8 @@
 use ratatui::widgets::WidgetRef;
 
-pub trait IsContent: WidgetRef {}
+pub trait IsContent: WidgetRef + CanBeFocused {}
+
+pub trait CanBeFocused {
+    fn focus(& mut self);
+    fn unfocus(& mut self);
+}
