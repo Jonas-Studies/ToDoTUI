cb0becfcbb1646a214c351b5f44b6452f243ce55
Jonas-Laptop <coding@jonas.zengerle.email>
2024-11-20 12:28:08 +0100

    Added the trait CanHandleUserinput

--- src/tui/traits.rs
+++ src/tui/traits.rs
@@ -1,6 +1,5 @@
 use ratatui::{
-    prelude::Position,
-    widgets::WidgetRef
+    crossterm::event::KeyCode, prelude::Position, widgets::WidgetRef
 };
 
 pub trait IsContent: WidgetRef + CanBeFocused + MayDisplayCursor {}
@@ -12,4 +11,8 @@ pub trait CanBeFocused {
 
 pub trait MayDisplayCursor {
     fn get_cursor_position (& self) -> Option<Position>;
+}
+
+pub trait CanHandleUserinput {
+    fn handle_userinput (& mut self, userinput: & KeyCode);
 }
