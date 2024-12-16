6411e415727179440659250ba3c111f92b4a05dc
Jonas-Laptop <coding@jonas.zengerle.email>
2024-12-16 12:34:29 +0100

    Changed the programm so that a new task only gets appended when its name is not empty

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,13 @@ fn main() {
 
     while let Some(index_of_selected_task) = tui::select_one_task(& tasks) {
         if index_of_selected_task == 0 {
-            tasks.insert(1, Task::new(String::new()));
-            tui::edit_one_task(& mut tasks[1]);
+            let mut new_task = Task::new(String::new());
+            
+            tui::edit_one_task(& mut new_task);
+
+            if new_task.get_name() != "" {
+                tasks.insert(1, new_task);
+            }
         }
         else {
             tui::edit_one_task(& mut tasks[index_of_selected_task]);
