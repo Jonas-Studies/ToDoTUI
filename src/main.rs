2ce86f1afae3a0302d93c368087669b7145f24e5
Jonas-Laptop <coding@jonas.zengerle.email>
2024-12-16 12:29:25 +0100

    Added functionality to add a new task

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,21 @@ mod tui;
 use task::Task;
 
 fn main() {
-    let mut tasks = if let Some(stored_tasks) = storage::get_tasks() {
-        stored_tasks
+    let mut tasks = vec![ Task::new(String::from("Add new task ...")) ];
+
+    if let Some(stored_tasks) = storage::get_tasks() {
+        tasks.extend(stored_tasks);
     }
-    else {
-        vec![ Task::new(String::from("Programm schreiben")) ]
-    };
 
     while let Some(index_of_selected_task) = tui::select_one_task(& tasks) {
-        tui::edit_one_task(& mut tasks[index_of_selected_task]);
+        if index_of_selected_task == 0 {
+            tasks.insert(1, Task::new(String::new()));
+            tui::edit_one_task(& mut tasks[1]);
+        }
+        else {
+            tui::edit_one_task(& mut tasks[index_of_selected_task]);
+        }
     }
 
-    storage::set_tasks(& tasks);
+    storage::set_tasks(& tasks.split_off(1));
 }
