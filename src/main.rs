db80c98d06d9d8f2106076aec7b95ffa0bef4865
Jonas-Laptop <coding@jonas.zengerle.email>
2025-01-10 12:41:42 +0100

    Implemented that when a new task get's created as finished, it get's moved to the second half

--- src/main.rs
+++ src/main.rs
@@ -14,11 +14,17 @@ fn main() {
     }
 
     while let Some(index_of_selected_task) = tui::select_one_task(tasks.reference_content()) {
+        // The first element is a placeholder, when selecting it a new task has to be created
         if index_of_selected_task == 0 {
             let new_task = Task::new(String::new());
             
             if let Some(new_task) = tui::edit_one_task(new_task) {
                 tasks.reference_content_mutable().insert(1, new_task);
+            }
+
+            // Finished tasks need to be placed in the second half
+            if tasks.reference_content()[1].is_finished() == true {
+                tasks.move_task_to_second_half(& 1);
             }
         }
         else {
@@ -26,6 +32,7 @@ fn main() {
                 if edited_task.is_finished() == true && tasks.reference_content()[index_of_selected_task].is_finished() == false {
                     tasks.reference_content_mutable()[index_of_selected_task] = edited_task;
 
+                    // Finished tasks need to be placed in the second half
                     tasks.move_task_to_second_half(& index_of_selected_task);
                 }
                 else {
