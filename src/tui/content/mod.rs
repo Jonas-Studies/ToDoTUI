c122156ae706f7023581f5621b45b7df10351ece
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 17:41:47 +0200

    Extended the content type to handle if one instance of content is supposed to handle userinput or not

--- src/tui/content/mod.rs
+++ src/tui/content/mod.rs
@@ -7,12 +7,13 @@ use types_of_content::TypesOfContent;
 
 pub struct Content {
     content: TypesOfContent,
-    can_be_focused: bool
+    can_be_focused: bool,
+    can_handle_userinput: bool
 }
 
 impl Content {
     pub fn new(content: TypesOfContent) -> Self {
-        Self { content, can_be_focused: false }
+        Self { content, can_be_focused: false, can_handle_userinput: false }
     }
     pub fn as_can_be_focused(mut self) -> Self {
         self.can_be_focused = true;
@@ -20,6 +21,13 @@ impl Content {
     }
     pub fn can_be_focused(&self) -> bool {
         self.can_be_focused.clone()
+    }
+    pub fn as_can_handle_userinput(mut self) -> Self {
+        self.can_handle_userinput = true;
+        self
+    }
+    pub fn can_handle_userinput(&self) -> bool {
+        self.can_handle_userinput.clone()
     }
 }
 
