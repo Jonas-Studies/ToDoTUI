c8fcc2fa8bfd394051cf442ec05f2715f8f917be
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 16:44:51 +0200

    Implemented the can be rendered trait for textinput

--- src/tui/content/types_of_content/textinput.rs
+++ src/tui/content/types_of_content/textinput.rs
@@ -1,3 +1,7 @@
+use ratatui::{prelude::{Buffer, Rect}, widgets::{Block, Paragraph, Widget}};
+
+use crate::tui::content::traits::CanBeRendered;
+
 pub struct Textinput {
     // Using a vector of chars instead of String because there would be problems with utf-8
     value: Vec<char>,
@@ -10,5 +14,19 @@ impl Textinput {
         let value = Vec::from_iter(value.chars());
 
         Self { value, title, cursor_offset: 0 }
+    }
+    fn get_value(&self) -> String {
+        String::from_iter(self.value.iter())
+    }
+    fn get_title(&self) -> String {
+        self.title.clone()
+    }
+}
+
+impl CanBeRendered for Textinput {
+    fn render (&self, area: Rect, buffer: &mut Buffer) {
+        Paragraph::new(self.get_value()).block(
+            Block::bordered().title(self.get_title())
+        ).render(area, buffer);
     }
 }
