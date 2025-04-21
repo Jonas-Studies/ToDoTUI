886161db07d3dc98e3ed7d497ea07e2d40b2024d
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 16:19:36 +0200

    Created a new trait for handling userinput

--- src/tui/content/traits.rs
+++ src/tui/content/traits.rs
@@ -1,4 +1,4 @@
-use ratatui::prelude::{Rect, Buffer};
+use ratatui::{crossterm::event::KeyCode, prelude::{Buffer, Rect}};
 
 pub trait CanBeRendered {
     fn render (&self, area: Rect, buffer: &mut Buffer);
@@ -6,4 +6,10 @@ pub trait CanBeRendered {
 
 pub trait CanBeFocused {
     fn render_focused (&self, area: Rect, buffer: &mut Buffer);
+}
+
+pub trait CanHandleUserinput<PossibleActions> {
+    // fn is supposed to return a type of an enum of possible actions to result from userinput.
+    // This is supposed to be getting defined at an controlling point in the Software.
+    fn handle_userinpt(&mut self, userinput: KeyCode) -> Option<PossibleActions>;
 }
