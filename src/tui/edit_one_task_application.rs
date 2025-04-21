36c42a1e5596eee7696a60ae5e3d40f01d61aefd
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 16:49:31 +0200

    Extended the edit one task app to contain the name as textinput

--- src/tui/edit_one_task_application.rs
+++ src/tui/edit_one_task_application.rs
@@ -2,7 +2,7 @@ use ratatui::{crossterm::event::KeyCode, layout::{Constraint, Layout}};
 
 use crate::task::Task;
 
-use super::content::{traits::{CanBeRendered, CanHandleUserinput}, types_of_content::{title::Title, TypesOfContent}, Content};
+use super::content::{traits::{CanBeRendered, CanHandleUserinput}, types_of_content::{textinput::Textinput, title::Title, TypesOfContent}, Content};
 
 pub struct Application {
     layout: Layout,
@@ -12,12 +12,17 @@ pub struct Application {
 
 impl Application {
     pub fn new(task_to_edit: Task) -> Self {
-        let layout = Layout::vertical( [ Constraint::Length(1) ] );
+        let layout = Layout::vertical(
+            [ Constraint::Length(1), Constraint::Length(3) ]
+        ).spacing(1).vertical_margin(1).horizontal_margin(3);
 
         let mut content = Vec::new();
 
         content.push(
             Content::new(TypesOfContent::Title(Self::get_title(task_to_edit.is_finished())))
+        );
+        content.push(
+            Content::new(TypesOfContent::Textinput(Textinput::new(task_to_edit.get_name(), String::from("Name"))))
         );
 
         Self { layout, task: task_to_edit, content }
