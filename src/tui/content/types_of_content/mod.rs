df6cd880a85c2d3cd5a9fc4d447b45c53d9a1ecd
Jonas-Laptop <coding@jonas.zengerle.email>
2025-04-21 16:45:07 +0200

    Implemented textinput as type of content

--- src/tui/content/types_of_content/mod.rs
+++ src/tui/content/types_of_content/mod.rs
@@ -1,4 +1,5 @@
 use ratatui::prelude::{Rect, Buffer};
+use textinput::Textinput;
 use title::Title;
 
 use super::traits::{CanBeFocused, CanBeRendered};
@@ -7,13 +8,15 @@ pub mod title;
 pub mod textinput;
 
 pub enum TypesOfContent {
-    Title(Title)
+    Title(Title),
+    Textinput(Textinput)
 }
 
 impl CanBeRendered for TypesOfContent {
     fn render (&self, area: Rect, buffer: &mut Buffer) {
         match self {
             TypesOfContent::Title(content) => { content.render(area, buffer); }
+            TypesOfContent::Textinput(content) => { content.render(area, buffer); }
         }
     }
 }
